a35a9c11b24f21490157e8bd388619166c8096fd
Nguyen Dac Tai <44217992+msc0d3@users.noreply.github.com>
2020-11-23 19:17:03 +0700

    Add files via upload

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtToken = New System.Windows.Forms.TextBox()
+        Me.txtCookie = New System.Windows.Forms.TextBox()
+        Me.txtUID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCopyToken = New System.Windows.Forms.Button()
+        Me.btnCopyCookie = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.PBar = New System.Windows.Forms.ToolStripProgressBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(600, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(72, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Get Token"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtToken
+        '
+        Me.txtToken.Location = New System.Drawing.Point(76, 58)
+        Me.txtToken.Multiline = True
+        Me.txtToken.Name = "txtToken"
+        Me.txtToken.Size = New System.Drawing.Size(518, 51)
+        Me.txtToken.TabIndex = 3
+        '
+        'txtCookie
+        '
+        Me.txtCookie.Location = New System.Drawing.Point(76, 127)
+        Me.txtCookie.Multiline = True
+        Me.txtCookie.Name = "txtCookie"
+        Me.txtCookie.Size = New System.Drawing.Size(518, 55)
+        Me.txtCookie.TabIndex = 4
+        '
+        'txtUID
+        '
+        Me.txtUID.Location = New System.Drawing.Point(76, 25)
+        Me.txtUID.Name = "txtUID"
+        Me.txtUID.Size = New System.Drawing.Size(518, 20)
+        Me.txtUID.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(37, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "UID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(23, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Token:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(20, 130)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Cookie:"
+        '
+        'btnCopyToken
+        '
+        Me.btnCopyToken.Location = New System.Drawing.Point(600, 57)
+        Me.btnCopyToken.Name = "btnCopyToken"
+        Me.btnCopyToken.Size = New System.Drawing.Size(75, 51)
+        Me.btnCopyToken.TabIndex = 10
+        Me.btnCopyToken.Text = "Copy"
+        Me.btnCopyToken.UseVisualStyleBackColor = True
+        '
+        'btnCopyCookie
+        '
+        Me.btnCopyCookie.Location = New System.Drawing.Point(600, 130)
+        Me.btnCopyCookie.Name = "btnCopyCookie"
+        Me.btnCopyCookie.Size = New System.Drawing.Size(75, 52)
+        Me.btnCopyCookie.TabIndex = 11
+        Me.btnCopyCookie.Text = "Copy"
+        Me.btnCopyCookie.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(13, 222)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(659, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Công cụ lấy cookie, token full quyền an toàn, không checkpoint từ app facebook tr" &
+    "ên điện thoại, máy ảo LDPlayer"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.PBar})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 315)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(697, 22)
+        Me.StatusStrip1.TabIndex = 13
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(527, 17)
+        Me.ToolStripStatusLabel1.Text = "Developed by Nguyen Huu Dat - J2TEAM Community - https://fb.com/groups/j2team.com" &
+    "munity"
+        '
+        'PBar
+        '
+        Me.PBar.Name = "PBar"
+        Me.PBar.Size = New System.Drawing.Size(100, 16)
+        Me.PBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.PBar.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 241)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(563, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "- Yêu cầu android đã được root, mở USB debug mode (Nếu không được hãy thử đổi kiể" &
+    "u kết nối USB trên điện thoại)"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(14, 259)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(392, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "- Chỉ mở 1 thiết bị khi lấy. VD nếu lấy từ điện thoại thì nên tắt máy ảo và ngược" &
+    " lại."
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(697, 337)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnCopyCookie)
+        Me.Controls.Add(Me.btnCopyToken)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUID)
+        Me.Controls.Add(Me.txtCookie)
+        Me.Controls.Add(Me.txtToken)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Get Token Full quyền v1.0"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtToken As TextBox
+    Friend WithEvents txtCookie As TextBox
+    Friend WithEvents txtUID As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCopyToken As Button
+    Friend WithEvents btnCopyCookie As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PBar As ToolStripProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,146 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim My_Process As New Process()
+    Dim My_Process_Info As New ProcessStartInfo()
+    Dim proc As System.Diagnostics.Process
+    Dim pProcess As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\cnd.exe"
+    Dim dll1 As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\AdbWinUsbApi.dll"
+    Dim dll2 As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\AdbWinApi.dll"
+    Dim fT As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\token.txt"
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PBar.Enabled = True
+        PBar.Visible = True
+        btnCopyToken.Text = "Copy"
+        btnCopyCookie.Text = "Copy"
+        txtUID.Text = ""
+        txtToken.Text = ""
+        txtCookie.Text = ""
+
+        BackgroundWorker1.RunWorkerAsync()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
+        For Each proc As Process In Process.GetProcessesByName("cnd")
+            proc.Kill()
+        Next
+        IO.File.WriteAllBytes(pProcess, My.Resources.adb)
+
+    End Sub
+
+    Public Function RunCharacterCheckASCII(ByVal s As String) As String
+        Dim str As String = s
+        Dim is_find As Boolean = False
+        Dim ch As Char
+        Dim ich As Integer = 0
+
+        Try
+            Dim schar As Char() = str.ToCharArray()
+
+            For i As Integer = 0 To schar.Length - 1
+                ch = schar(i)
+                ich = Microsoft.VisualBasic.AscW(ch)
+
+                If ich > 127 Then ' not ascii or extended ascii
+                    is_find = True
+                    schar(i) = "?"c
+                End If
+            Next
+
+            If is_find Then str = New String(schar)
+        Catch ex As Exception
+        End Try
+
+        Return str
+    End Function
+
+    Private Sub btnCopyToken_Click(sender As Object, e As EventArgs) Handles btnCopyToken.Click
+        My.Computer.Clipboard.SetText(txtToken.Text)
+        btnCopyToken.Text = "Đã copy"
+    End Sub
+
+    Private Sub btnCopyCookie_Click(sender As Object, e As EventArgs) Handles btnCopyCookie.Click
+        My.Computer.Clipboard.SetText(txtCookie.Text)
+        btnCopyCookie.Text = "Đã copy"
+    End Sub
+
+    Private Sub ToolStripStatusLabel1_Click(sender As Object, e As EventArgs) Handles ToolStripStatusLabel1.Click
+
+    End Sub
+
+
+    Function Decrypt(text As String, password As String) As String
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
+        Dim decrypted As String = ""
+        Dim hash(31) As Byte
+        Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+        Array.Copy(temp, 0, hash, 0, 16)
+        Array.Copy(temp, 0, hash, 15, 16)
+        AES.Key = hash
+        AES.Mode = Security.Cryptography.CipherMode.ECB
+        Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
+        Dim Buffer As Byte() = Convert.FromBase64String(text)
+        decrypted = System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+        Return decrypted
+    End Function
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim sArgs As String = "/c adb pull /data/data/com.facebook.katana/app_light_prefs/com.facebook.katana/authentication " & fT
+        Console.WriteLine(sArgs)
+        Dim startInfo As New ProcessStartInfo("cmd.exe")
+        startInfo.WindowStyle = ProcessWindowStyle.Minimized
+        startInfo.Arguments = sArgs
+        startInfo.RedirectStandardOutput = True
+        startInfo.UseShellExecute = False
+        startInfo.CreateNoWindow = True
+        Dim p = Process.Start(startInfo)
+        Dim result = p.StandardOutput.ReadToEnd()
+        p.Close()
+        If My.Computer.FileSystem.FileExists(fT) Then
+            Dim t As String = My.Computer.FileSystem.ReadAllText(fT)
+            My.Computer.FileSystem.DeleteFile(fT)
+            t = Regex.Replace(t, "[^\u0020-\u007E]", "|")
+
+            Dim m As Match = Regex.Match(t, "EAA(.*?)\|", RegexOptions.IgnoreCase)
+            If (m.Success) Then
+                Dim key As String = m.Groups(1).Value
+                txtToken.Text = "EAA" & key
+            Else
+                MsgBox("Không tìm thấy Token. Bạn hãy chắc chắn đã làm đúng các bước?")
+            End If
+
+            Dim m2 As Match = Regex.Match(t, "\[\{(.*?)\}\]", RegexOptions.IgnoreCase)
+            If (m2.Success) Then
+                Dim key As String = m2.Groups(0).Value
+                txtCookie.Text = key
+            End If
+
+            Dim m3 As Match = Regex.Match(t, "uid\|\|(.*?)\|", RegexOptions.IgnoreCase)
+            If (m3.Success) Then
+                Dim key As String = m3.Groups(1).Value
+                txtUID.Text = key
+            End If
+
+        Else
+            MsgBox("Không tìm thấy Token. Bạn hãy chắc chắn đã làm đúng các bước??")
+        End If
+
+
+        PBar.Enabled = False
+        PBar.Visible = False
+
+        For Each proc As Process In Process.GetProcessesByName("cnd")
+            'proc.Kill()
+        Next
+    End Sub
+
+    Private Sub StatusStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles StatusStrip1.ItemClicked
+
+    End Sub
+End Class
